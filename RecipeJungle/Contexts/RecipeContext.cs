aeec977551d235e64c4fdd1b215500c37147d712
SemihCatal <catalsemih03@gmail.com>
2019-06-15 15:13:56 +0300

    Fixing RecipeContext

--- RecipeJungle/Contexts/RecipeContext.cs
+++ RecipeJungle/Contexts/RecipeContext.cs
@@ -23,6 +23,8 @@ namespace RecipeJungle.Contexts
             mb.Entity<RecipeTag>().HasKey(x => new { x.RecipeId, x.TagId });
 
             mb.Entity<Tag>().HasIndex(x => x.Text).IsUnique();
+
+            mb.Entity<Recipe>().HasOne(x => x.User).WithMany(x => x.RecipesOfUser);
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
