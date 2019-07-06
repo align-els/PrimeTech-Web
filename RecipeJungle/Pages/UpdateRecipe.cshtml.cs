00c7471e8263c4db812a68c3397c2fab1094d6fe
SemihCatal <catalsemih03@gmail.com>
2019-07-06 22:08:01 +0300

    Update Recipe Implementation

---
+++ RecipeJungle/Pages/UpdateRecipe.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace RecipeJungle.Pages
+{
+    public class UpdateRecipeModel : RecipePageModel
+    {
+        public RecipeJungle.Services.IRecipeService _recipeService;
+        public RecipeJungle.Entities.Recipe Recipe;
+
+        public UpdateRecipeModel(RecipeJungle.Services.IRecipeService recipeService)
+        {
+            _recipeService = recipeService;
+        }
+
+        public void OnGet(int Id)
+        {
+            //Recipe = _recipeService.GetReceiveById(Id);
+        }
+    }
+}
