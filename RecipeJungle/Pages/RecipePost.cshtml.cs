650a8364595e50302ddced0e255ac360dac76310
Tuba Yildiz <yldztubanur12@gmail.com>
2019-07-12 20:32:34 +0300

    Recipe Details page implementation

---
+++ RecipeJungle/Pages/RecipePost.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RecipeJungle.Entities;
+using RecipeJungle.Services;
+
+
+namespace RecipeJungle.Pages
+{
+    public class RecipePostModel : RecipePageModel
+    {
+        public IRecipeService _recipeService;
+        public Recipe Recipe;
+       
+
+        public RecipePostModel(IRecipeService recipeService)
+        {
+            _recipeService = recipeService;
+        }
+
+        public void OnGet(int Id)
+        {
+            Recipe = _recipeService.GetReceiveById(Id);
+        }
+    }
+}
