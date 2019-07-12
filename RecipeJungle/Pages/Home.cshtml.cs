33d0cc9841efd6d0ca7b6d2a94c1c9cf43f11205
beyzaaltuntas <baltuntas@etu.edu.tr>
2019-07-12 21:16:52 +0300

    bugs fixed

--- RecipeJungle/Pages/Home.cshtml.cs
+++ RecipeJungle/Pages/Home.cshtml.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RecipeJungle.Entities;
+using RecipeJungle.Services;
 
 namespace RecipeJungle.Pages
 {
     public class HomeModel : RecipePageModel
     {
+        public Services.IRecipeService _recipeService;
+        public Recipe recipe;
+        public List<Recipe> recipes;
+
+        public HomeModel(Services.IRecipeService recipeService)
+        {
+            _recipeService = recipeService;
+        }
+
         public void OnGet()
         {
+            recipes = _recipeService.ListRecipes();
         }
     }
 }
