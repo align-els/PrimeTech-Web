24e411644dcb4cf9583795af32a1afc8a9283a7d
align-els <aligng1620@gmail.com>
2019-07-23 20:04:54 +0300

    debugging enablwd

--- RecipeJungle/Middlewares/ActionFailureHandler.cs
+++ RecipeJungle/Middlewares/ActionFailureHandler.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 using Newtonsoft.Json;
 using RecipeJungle.Exceptions;
 
@@ -16,8 +18,23 @@ namespace RecipeJungle.Middlewares
         }
 
         public async Task Invoke(HttpContext context) {
+
+            var requestBodyStream = new MemoryStream();
+            var originalRequestBody = context.Request.Body;
+
+            await context.Request.Body.CopyToAsync(requestBodyStream);
+            requestBodyStream.Seek(0, SeekOrigin.Begin);
+
+            var url = UriHelper.GetDisplayUrl(context.Request);
+            var requestBodyText = new StreamReader(requestBodyStream).ReadToEnd();
+            Console.Error.WriteLine($"[DEBUG-LOG] {context.Request.Method} {url} {requestBodyText}");
+
+            requestBodyStream.Seek(0, SeekOrigin.Begin);
+            context.Request.Body = requestBodyStream;
+
             try {
                 await next(context);
+                context.Request.Body = originalRequestBody;
             }
             catch (ActionFailedException e) {
                 context.Response.ContentType = "application/json";
